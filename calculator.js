33e60cd8bf1661a4703cbc4cd62ec65c63bc2d7d
Derrick Kityo <33717684+derrickwod@users.noreply.github.com>
2017-12-14 10:08:34 +0000

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -29,46 +29,6 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-function hideKeyboard() {
-  //this set timeout needed for case when hideKeyborad
-  //is called inside of 'onfocus' event handler
-  setTimeout(function() {
-
-    //creating temp field
-    var field = document.createElement('input');
-    field.setAttribute('type', 'text');
-    //hiding temp field from peoples eyes
-    //-webkit-user-modify is nessesary for Android 4.x
-    field.setAttribute('style', 'position:absolute; top: 0px; opacity: 0; -webkit-user-modify: read-write-plaintext-only; left:0px;');
-    document.body.appendChild(field);
-
-    //adding onfocus event handler for out temp field
-    field.onfocus = function(){
-      //this timeout of 200ms is nessasary for Android 2.3.x
-      setTimeout(function() {
-
-        field.setAttribute('style', 'display:none;');
-        setTimeout(function() {
-          document.body.removeChild(field);
-          document.body.focus();
-        }, 14);
-
-      }, 200);
-    };
-    //focusing it
-    field.focus();
-
-  }, 50);
-}
-
-function (event) {
-    if (event.which == 13 || event.keyCode == 13) {
-        hideKeyboard()
-        return false;
-    }
-    return true;
-};
-
 var typeStyle = getParameterByName("t");
 
 if(typeStyle != '')

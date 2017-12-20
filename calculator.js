8f9aab139d928114ab2c336b7a0fca1ec8864741
Derrick Kityo <33717684+derrickwod@users.noreply.github.com>
2017-12-20 11:17:41 +0000

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -171,7 +171,7 @@ $(document).ready(function()
 	
 	$(".q5-next").click(function()
 	{
-		$("#no-toughening-2").prop("checked", true);
+		//$("#no-toughening-2").prop("checked", true);
 		setTimeout(function()
 		{
 			if(productType != "vintage" && productType != "mottled" && productType != "speckled" && productType != "mercury")

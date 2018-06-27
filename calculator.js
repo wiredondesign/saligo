5d3270b776db050e105dcd237430d616b831fcc8
Derrick Kityo <33717684+derrickwod@users.noreply.github.com>
2018-06-27 16:14:59 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -185,7 +185,6 @@ $(document).ready(function()
 		$("#no-toughening-2").prop("checked", true);
 		$("#polished-edge-2").prop("checked", true);
 		
-		$(".question-5").hide();
 		$(".question-6").hide();
 		$(".question-7").show();
 		
